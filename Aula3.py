f88172ceadefb7848f7f550ce2da5f3b7785d8a7
Caue Dal Berto Rathke <caue.rathke@pampa.compasso>
2020-03-26 12:54:58 -0300

    Aula3 commit

---
+++ Aula3.py
@@ -0,0 +1,73 @@
+class Data:
+
+    def __init__(self, dia, mes, ano):
+        if(dia > 31 or dia < 0):
+            dia = 0
+        if(mes > 12):
+            mes = 0
+        self.__dia = dia   
+        self.__mes = mes
+        self.__ano = ano
+
+    def formatada(self):
+        print(self.__dia,self.__mes,self.__ano, sep='/')
+
+data = Data(23,12,2000)
+data.formatada()
+
+#funciona
+data.teste = 8
+print(data.teste)
+
+class Conta:
+
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto ... {}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
+    def saca(self, valor):
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def get_titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001', 'Caixa':'104', 'Bradesco':'237'}
