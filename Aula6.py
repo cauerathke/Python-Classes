d894b3384f3f3d355b9ef2b42d993859132ebbe4
Caue Dal Berto Rathke <caue.rathke@pampa.compasso>
2020-03-31 15:21:21 -0300

    Comiit2 aula6

--- Aula6.py
+++ Aula6.py
@@ -21,6 +21,42 @@ class ValidaCpf:
     def __str__(self):
         return self.format_cpf()
 
-cpf = ValidaCpf(111111111)
-print(cpf)
+#cpf = ValidaCpf(111111111)
+#print(cpf)
  
+#factory
+class Documento:
+    @staticmethod
+    def criar_novo(documento):
+        doc_str = str(documento)
+        if len(doc_str) == 11:
+            return DocCpf(doc_str)
+        if len(doc_str) == 14:
+            return DocCnpj(doc_str)
+        if len(doc_str) == 20:
+            return DocQualquer(doc_str)
+        raise ValueError('Documento incorreto!')
+
+class DocCpf:
+    pass
+class DocCnpj:
+    pass
+class DocQualquer:
+    pass
+
+#regex 
+import re
+
+padrao = "[0-9]{2}[0-9]{4,5}[0-9]{4}"
+texto = "eu gosto do numero 12321323331 w[asdka"
+resposta = re.findall(padrao, texto)
+
+print(resposta)
+
+# []	Define um range ou um grupo de caracteres	[0-9]; [a-z]; [abc]
+# *	Marca nenhuma, uma ou mais ocorrências	sol*
+# {}	Quantidade de repetições de uma ocorrência definida	[abc]{5}
+# \d	Qualquer número de 0 até 9	\d{3,4}
+# \w	Qualquer número de a té 9 letra de a até z ou _	\w{10}
+# |	Um ou outro	@$
+# ()	Captura e agrupa	(\w{4})
